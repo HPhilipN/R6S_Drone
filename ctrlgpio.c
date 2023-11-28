5696b7503e32de097113fb359289eb2da8d08589
HPhilipN <nophyswaffles@gmail.com>
2023-11-28 15:59:50 -0800

    ctrl gpio 90% done: needs pwm i think

--- ctrlgpio.c
+++ ctrlgpio.c
@@ -1,28 +1,95 @@
 #include <stdio.h>
-#include <time.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/epoll.h>
 #include <fcntl.h>
+#include <ctrlgpio.h>
+
+//gpio sys directory
+#define pinPath "/sys/class/gpio"
+#define pinExport "/export"
+#define pinUnExport "/unexport"
+#define pinValue "/value"
+#define pinDir "/direction"
+#define pinEdge "/edge"
+
+//pin values
+#define dirIn "in"
+#define dirOut "out"
+#define hi "0"
+#define lo "0"
 
 
+// //write to file 
+// static void writeNreadToFile(const char* fileName, const char* value)
+// {
+// 	FILE *pFile = fopen(fileName, "w");
+// 	fprintf(pFile, "%s", value);
+// 	fclose(pFile);
+// }
 
-//write to file 
-static void writeToFile(const char* fileName, const char* value)
+// //reads file
+// static int readLineFromFile(char* fileName, char* buff, unsigned int maxLength)
+// {
+// 	FILE *file = fopen(fileName, "r");
+// 	int bytes_read = getline(&buff, &maxLength, file);
+// 	fclose(file);
+// 	return bytes_read;
+// }
+
+static int readNwriteFile(const char *fname, const char *wdata)
 {
-	FILE *pFile = fopen(fileName, "w");
-	fprintf(pFile, "%s", value);
-	fclose(pFile);
+	int fd;
+	fd = open(fname, O_WRONLY | O_NONBLOCK);
+	if(fd < 0)
+		printf("cannot open file %s...%d\r\n", fname, fd);
+
+	write(fd, wdata, strlen(wdata));
+	close(fd);
+	return 0;
 }
 
-//reads file
-static int readLineFromFile(char* fileName, char* buff, unsigned int maxLength)
+
+int gpioExport(int gpio_pin)
 {
-	FILE *file = fopen(fileName, "r");
-	int bytes_read = getline(&buff, &maxLength, file);
-	fclose(file);
-	return bytes_read;
+	char gpio_str[4];
+	sprintf(gpio_str, "%d" ,gpio_pin);
+	return readNwriteFile(pinPath pinExport, gpio_str);
 }
+
+int gpioUnExport(int gpio_pin)
+{
+	char gpio_str[4];
+	sprintf(gpio_str, "%d" ,gpio_pin);
+	return readNwriteFile(pinPath pinUnExport, gpio_str);
+}
+
+int setDirection(int gpio_pin, const char* dir)
+{
+	char path_str[40];
+	sprintf(path_str, "%s/gpio%d%s", pinPath, gpio_pin, pinDir);
+	return readNwriteFile(path_str, dir);
+}
+
+int setValue(int gpio_pin, const char* value)
+{
+	char path_str[40];
+	sprintf(path_str, "%s/gpio%d%s", pinPath, gpio_pin, pinValue);
+	return readNwriteFile(path_str, value);
+}
+
+int pinFdtoVal(int gpio_pin)
+{
+	int fd;
+	char fname[40];
+	sprintf(fname, "%s/gpio%d%s", pinPath, gpio_pin, pinValue);
+	fd = open(fname, O_RDONLY | O_NONBLOCK);
+	if(fd < 0)
+		printf("could not open file %s...%d\r\n", fname, fd);
+		
+	return fd; 
+}
+
 

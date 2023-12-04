5a6bc030ca3a458be0f7dcfc932adde658dfb7d8
Astravius <astravius@outlook.com>
2023-12-04 15:47:01 -0800

    clang

--- CAR/main.c
+++ CAR/main.c
@@ -124,6 +124,10 @@ int main(){
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("error initializing SDL: %s\n", SDL_GetError());
     }
+    SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       1000, 1000, 0);
 
     while(1){
 

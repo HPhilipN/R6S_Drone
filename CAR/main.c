6be879f3edb08d258f2cb9b3f29a4fe0ced1f09a
Astravius <astravius@outlook.com>
2023-12-04 16:46:40 -0800

    a

--- CAR/main.c
+++ CAR/main.c
@@ -124,10 +124,7 @@ int main(){
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("error initializing SDL: %s\n", SDL_GetError());
     }
-    SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
-                                       SDL_WINDOWPOS_CENTERED,
-                                       SDL_WINDOWPOS_CENTERED,
-                                       1000, 1000, 0);
+    SDL_Window* win = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, SDL_WINDOW_INPUT_GRABBED);
 
     int close = 0;
     while(!close){

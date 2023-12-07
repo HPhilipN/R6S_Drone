2ab59c6639ebb31d81be3a912acac9dd0467578a
Astravius <astravius@outlook.com>
2023-12-07 14:10:10 -0800

    tcp test

--- Server/server.c
+++ Server/server.c
@@ -19,8 +19,57 @@
 #include <time.h>
 #include <pthread.h>
 
+#define PORT 8080
+
 int main(){
 
-    SDLNet_TCP_Send();
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size;
+
+    // Create socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address structure
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind the socket
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Error binding");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_socket, 10) == -1) {
+        perror("Error listening");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    // Accept connection from client
+    addr_size = sizeof(client_addr);
+    client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_size);
+    if (client_socket == -1) {
+        perror("Error accepting connection");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive data from client
+    int received_data;
+    recv(client_socket, &received_data, sizeof(received_data), 0);
+    printf("Received data from client: %d\n", received_data);
+
+    // Close sockets
+    close(client_socket);
+    close(server_socket);
+
+    return 0;
 
 }

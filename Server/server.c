62622812d4bc9124776cf83324adc49f07493ae9
Astravius <astravius@outlook.com>
2023-12-07 13:19:57 -0800

    cleanup

--- Server/server.c
+++ Server/server.c
@@ -1,11 +1,26 @@
-
-
-
 /*
 
     Connect to
 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdbool.h>
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_net.h>
 
+#include <time.h>
+#include <pthread.h>
+
+int main(){
+
+    SDLNet_TCP_Send();
+
+}
